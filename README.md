73e44b512ff7d3eef86abcbdde145e155862966f
Chris Wilson <cpwilson87@outlook.com>
2025-06-12 09:32:28 +0100

    feat: add Gemini agent to fix bugs in python code

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Kaa
 
 A simple AI agent written in Python
+
+## TODO
+
+- Come up with a better name...
+    - AskPy?
+    - Typhon (type + python)
+    - Nagabot (hindu / budhist lore)
--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import os
 import sys
+
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+
+from prompts import system_prompt
+from call_function import available_functions, call_function
+from config import MAX_ITERS
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -27,19 +32,60 @@ def main():
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)])
     ]
+    iters = 0
+    while True:
+        iters += 1
+        if iters > MAX_ITERS:
+            print(f"Maximum iterations ({MAX_ITERS}) reached.")
+            sys.exit(1)
 
-    generate_content(client=client, messages=messages, user_prompt=user_prompt, verbose=verbose)
+        try:
+            final_response = generate_content(client, messages, verbose)
+            if final_response:
+                print("Final response:")
+                print(final_response)
+                break
+        except Exception as e:
+            print(f"Error in generate_content: {e}")
 
 
-def generate_content(client, messages, user_prompt, verbose):
+
+def generate_content(client, messages, verbose):
     response = client.models.generate_content(
-            model=model, contents=messages
+            model=model, 
+            contents=messages,
+            config=types.GenerateContentConfig(
+                tools=[available_functions], system_instruction=system_prompt
+                )
     )
-    print("Response:")
-    print(response.text)
+
+    if response.candidates:
+        for candidate in response.candidates:
+            function_call_content = candidate.content
+            messages.append(function_call_content)
+
+    if not response.function_calls:
+        return response.text
+
+    function_responses = []
+
+    for function_call_part in response.function_calls:
+        function_call_result = call_function(function_call_part, verbose)
+        if (
+            not function_call_result.parts
+            or not function_call_result.parts[0].function_response
+        ):
+            raise Exception("empty function call result")
+        if verbose:
+            print(f"-> {function_call_result.parts[0].function_response.response}")
+        function_responses.append(function_call_result.parts[0])
+
+    if not function_responses:
+        raise Exception("no function responses generated, exiting.")
+
+    messages.append(types.Content(role="tool", parts=function_responses))
 
     if verbose:
-        print(f"User prompt: {user_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
